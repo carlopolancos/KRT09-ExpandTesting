4f006beb31218711b360c2699d4f3f42af7e6f1c
Carlo James Polancos <carlo.polancos@awsys-i.com>
2026-03-05 16:15:08 +0800

    Removed unused imports

--- src/test/java/com/api/automation/ParallelBuilder.java
+++ src/test/java/com/api/automation/ParallelBuilder.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.Runner.Builder;
-import com.intuit.karate.junit5.Karate;
 
 public class ParallelBuilder {
 	
--- src/test/java/com/api/automation/ParallelBuilderWithTags.java
+++ src/test/java/com/api/automation/ParallelBuilderWithTags.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit5.Karate;
 
 public class ParallelBuilderWithTags {
 	
